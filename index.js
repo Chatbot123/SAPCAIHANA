ce7ccd7c8e9ddd14d22daec2ff7f573e37ba0d51
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-04-17 12:06:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const bodyParser = require('body-parser')
+
+const app = express() 
+
+app.use(bodyParser.json()) 
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+  res.send({
+    replies: [{
+      type: 'text',
+      content: 'Roger that',
+    }], 
+    conversation: {
+      memory: { key: 'value' }
+    }
+  })
+})
+
+app.post('/errors', (req, res) => {
+  console.log(req.body) 
+  res.send() 
+}) 
+
+app.listen(PORT, () => { 
+  console.log('Server is running on port 5000') 
+})
