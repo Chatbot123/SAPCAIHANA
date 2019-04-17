be2776cdfac92b1b39401a0044afc5a30b7c5b71
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-04-17 16:16:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 app.post('/getmeasure-state', (req, res) => {
   console.log(req.body)
   
-   var ent_measure = req.body.entities.ent_measure[0].raw;
-  var ent_state_value = req.body.entities.ent_state_value[0].raw;
-  var ent_state = req.body.entities.ent_state[0].raw;
+   var v_ent_measure = req.body.entities.ent_measure[0].raw;
+  var v_ent_state_value = req.body.entities.ent_state_value[0].raw;
+  var v_ent_state = req.body.entities.ent_state[0].raw;
   
-//var content_text = ent_measure ;//+ " " + ent_state_value + " " + ent_state;
+var content_text = v_ent_measure ;//+ " " + ent_state_value + " " + ent_state;
 //`${content_text}`
   res.send({
     replies: [{
       type: 'text',
-      content: ent_measure
+      content: content_text,
     }], 
     conversation: {
       memory: { key: 'value' }
