e348bc45ef18f8cde29cdf838bccca575d2185a2
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-04-29 15:17:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const recastai = require('recastai').default;
+var sapcai = require('sapcai').default
+
+var request = new sapcai.request(f6f1cfd675c26656fef9a2367f62c4a4, 'en')
+//const recastai = require('recastai').default;
 const requestify = require('requestify');
-const request = new recastai.request('f6f1cfd675c26656fef9a2367f62c4a4');
+//const request = new recastai.request('f6f1cfd675c26656fef9a2367f62c4a4');
 
 const app = express() 
 
